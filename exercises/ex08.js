64ea630d99e9542a512afc7ed242f46ae92ec0e4
Jose Muzquiz <jamuzquizgarcia@gmail.com>
2024-09-19 11:58:36 -0700

    Completed exercise 8

--- exercises/ex08.js
+++ exercises/ex08.js
@@ -15,8 +15,24 @@ You can check if a number is divisible by another number by checking if x % y ==
 
 var x = 1;
 
-while (/* your stop condition goes here */) {
+while (x <= 100) {
+    if ((x % 3 === 0) && (x % 5 === 0)) {
+        console.log(`${x} = FizzBuzz`)
+    }
+    else if (x % 3 === 0) {
+        console.log(`${x} = Fizz`)
+    }
+    else if (x % 5 === 0) {
+        console.log(`${x} = Buzz`)
+    }
+    else {
+        console.log(x)
+    }
+
+    x++
+    
     // check divisibility
     // print Fizz, Buzz, or FizzBuzz
     // increment x
 }
+
